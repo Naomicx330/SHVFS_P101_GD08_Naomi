b6e6413fe7c2660767141c4155ff34ac5bf59c2c
Naomicx330 <115709283+Naomicx330@users.noreply.github.com>
2022-11-10 20:27:32 +0800

    111

---
+++ Quizzes/SHVFS_P101_GD08_Q2022-11-10_Naomi/SHVFS_P101_GD08_Q2022-11-10_Naomi/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace SHVFS_P101_GD08_Naomi
+{
+    public class program
+    {
+        public static void Main()
+        {
+            int a = 1, word,name,b;
+            Console.WriteLine("HEllo,please enter your name.");
+            name = Convert.ToInt32(Console.ReadLine());
+
+
+             word= 1002;
+             = 1;
+            Console.WriteLine("please enter your guess");
+            while 
+            {
+                = Convert.ToInt32(Console.ReadLine());
+                if 
+                {
+                    Console.WriteLine("");
+                }
+                else if 
+                {
+                    Console.WriteLine("");
+                }
+
+
+
+            }
+    }
+}
