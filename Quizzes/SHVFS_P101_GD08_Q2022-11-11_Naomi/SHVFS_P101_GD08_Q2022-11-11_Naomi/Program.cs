16f74d60922ea65744f4f1e4801e454c70649c90
Naomicx330 <115709283+Naomicx330@users.noreply.github.com>
2022-11-11 20:10:28 +0800

    111

---
+++ Quizzes/SHVFS_P101_GD08_Q2022-11-11_Naomi/SHVFS_P101_GD08_Q2022-11-11_Naomi/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace SHVFS_P101_GD08_Naomi
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+        public class Game
+        {
+            public int X;
+            public int Y;
+            public int Z;
+            public Game() { }
+
+        }
+        public Game (int x,int y,int z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+        public 
+    }
+}
